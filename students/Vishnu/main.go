eb95914f4952eb1a580981d39ffee28e37926ebc
Vishnu Ellusamy <vlh1cob@bosch.com>
2024-11-04 20:23:09 +0530

    finishing the okish code

--- students/Vishnu/main.go
+++ students/Vishnu/main.go
@@ -6,15 +6,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello Hello! Welcome to the quiz game")
 
 	fileName := flag.String("fileName", "problems.csv", "a file name in csv format")
-	flag.Parse()
-
-	timeout = flag.Int("timeout", 30, "time limit for the quiz, if it exceeds for a question, it exits program")
+	timeout := flag.Int("timeout", 30, "time limit for the quiz, if it exceeds for a question, it exits program")
 	flag.Parse()
 
 	records := readcsvfiles(*fileName)
@@ -22,12 +21,27 @@ func main() {
 	var marks int
 
 	for _, value := range records {
-
-		fmt.Printf("Question: %s = ", value[0])
 		var userAnswer string
-		fmt.Scanln(&userAnswer)
-		if userAnswer == value[1] {
-			marks++
+		fmt.Printf("Question: %s = ", value[0])
+		timer := time.NewTimer(time.Duration(*timeout) * time.Second)
+
+		answerCh := make(chan string)
+		go func() {
+			fmt.Scanln(&userAnswer)
+			answerCh <- userAnswer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("Time out!")
+			fmt.Println("You scored", marks, "out of", len(records))
+			return
+		case userAnswer := <-answerCh:
+			if userAnswer == value[1] {
+				fmt.Scanln(&userAnswer)
+				marks++
+			}
+			timer.Stop()
 		}
 
 	}
